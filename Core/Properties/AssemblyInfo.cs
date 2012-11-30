c223aec6b6478c97179ffd2374ecba94a9e136e8
Pavel Perestoronin <eigenein@gmail.com>
2012-11-30 21:48:13 +0300

    Core library

---
+++ Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Skype Ninja Core Classes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Skype Ninja")]
+[assembly: AssemblyCopyright("Pavel Perestoronin © 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d78db714-bca4-41d1-baae-123467831d97")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
