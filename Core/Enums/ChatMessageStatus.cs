3baa67ba558731f22fa576401084239488b8f78a
Pavel Perestoronin <eigenein@gmail.com>
2012-12-17 02:59:00 +0300

    Core enums

---
+++ Core/Enums/ChatMessageStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eigenein.SkypeNinja.Core.Enums
+{
+    internal enum ChatMessageStatus
+    {
+        Sending = 1,
+        Sent = 2,
+        Received = 3,
+        Read = 4
+    }
+}
---
+++ Core/Enums/ChatMessageType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace eigenein.SkypeNinja.Core.Enums
+{
+    internal enum ChatMessageType
+    {
+        AddedMembers = 1,
+        CreatedChatWith = 2,
+        Said = 3,
+        Left = 4,
+        SetTopic = 5
+    }
+}
