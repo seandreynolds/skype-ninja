834771b272cbee706b8c5fc1bf222496bc6e8de9
Pavel Perestoronin <eigenein@gmail.com>
2013-01-21 03:31:29 +0300

    CopyingStatistics

---
+++ Core/Common/CopyingStatistics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eigenein.SkypeNinja.Core.Common
+{
+    /// <summary>
+    /// Represents the copying process statistics.
+    /// </summary>
+    public class CopyingStatistics
+    {
+        // TODO.
+    }
+}
